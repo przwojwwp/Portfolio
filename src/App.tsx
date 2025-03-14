2d162fce13bc93fae00f200e431ed761b551b6d4
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-03-14 12:14:52 +0100

    feat: introduce basic button for canvas matrix rain background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { MatrixRain } from "./components/MatrixRain/MatrixRain";
 import { ProjectsList } from "./components/ProjectsList/ProjectsList";
 
 export function App() {
+  const [isRaining, setIsRaining] = useState(false);
+
+  const handleMatrixRain = () => {
+    setIsRaining(!isRaining);
+  };
+
   return (
     <>
+      <button onClick={handleMatrixRain}>
+        {isRaining ? "Stop The Rain" : "Let's Raining"}
+      </button>
       <Header />
       <ProjectsList />
 
-      <MatrixRain />
+      {isRaining && <MatrixRain />}
     </>
   );
 }
