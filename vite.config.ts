9635024872ee7e528965842737e9feff9cb49954
wojciechprzyluski <przyluskiwojciech@gmail.com>
2025-03-20 14:09:23 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
